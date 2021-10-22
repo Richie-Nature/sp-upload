38b020bced1339a29f6e3973620f27f4f175fbf7
Imoh Obot <obotimoh20@gmail.com>
2021-10-22 19:55:15 +0100

    initial commit

---
+++ src/main/java/com/richie/practice/uploads/UploadsApplication.java
@@ -0,0 +1,13 @@
+package com.richie.practice.uploads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richie/practice/uploads/UploadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richie.practice.uploads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
